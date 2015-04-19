0546d05d293d083a2c48a960bb4adc1783500a75
Ilya V. Matveychikov <i.matveychikov@securitycode.ru>
2015-04-19 22:48:28 +0300

    Dump accounted data to the console log

--- nodejs/app.js
+++ nodejs/app.js
@@ -7,34 +7,26 @@ var app = express();
   
 app.get('/stamina', function (req, res) {
 
-    res.writeHead(200, { 'Content-Type' : 'text/html' });
+	res.writeHead(200, { 'Content-Type' : 'text/html' });
 
-    fs.open('/dev/stamina', 'r', function(status, fd) {
-	if (status) {
-		console.log(status.message);
-		return;
-	}
-/*
+	fs.readFile('/dev/stamina', function (err, data) {
+		if (err) {
+			console.log(err);
+		} else {
+			var syscall = 0;
 
-  core dumps :(
+			binary.parse(data).loop(function(end, vars){
+				this.word64lu('usage').word64lu('stack_min').word64lu('stack_max').tap(function (vars) {
+					vars.syscall = syscall++;
+					console.dir(vars);
+				})
+				if (this.eof()) end();
+			})
+		}
 
-	var buffer = new Buffer(16384);
-	fs.read(fd, buffer, 0, 16384, 0, function(err, nbytes, buffer){
-
-		binary.parse(buffer).loop(function(end, vars){
-			if (this.eof()) {
-				end();
-			}
-		});
-
-		console.log(nbytes);
-    		res.end();
-	});
-    });
-
-*/
-
-});
+		res.end();
+	})
+})
 
 var server = app.listen(3000, function () {
 });

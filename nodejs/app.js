27ac42b7f86f6c560eaa1e19607131b112a31f69
Ilya V. Matveychikov <i.matveychikov@securitycode.ru>
2015-04-21 23:58:01 +0300

    nodejs: read syscalls.list and manage syscallUsage object

--- nodejs/app.js
+++ nodejs/app.js
@@ -8,24 +8,46 @@ var app = express();
 app.set('views', './views')
 app.set('view engine', 'jade');
 
+//
+// 1. read system call table (sync)
+//
+
+var syscallTable = new Object;
+fs.readFileSync('syscalls.list').toString().split('\n').forEach(function (line) {
+	if (!line) return;
+	var s = line.split(" "); // __NR_read 0 ...
+	syscallTable[ s[1] ] = s[0];
+});
+
+//
+// 2. read system call usage information (async)
+//
+
+var syscallUsage = new Object;
 app.get('/stamina', function (req, res) {
 
 	res.writeHead(200, { 'Content-Type' : 'text/html' });
+
+	syscallUsage = {};
 
 	fs.readFile('/dev/stamina', function (err, data) {
 		if (err) {
 			console.log(err);
 		} else {
 			var syscall = 0;
-
 			binary.parse(data).loop(function(end, vars){
 				this.word64lu('usage').word64lu('stack_min').word64lu('stack_max').tap(function (vars) {
-					vars.syscall = syscall++;
-					console.dir(vars);
+					syscallUsage[syscall] = {};
+					syscallUsage[syscall]['usage'] = vars['usage'];
+					syscallUsage[syscall]['stack_min'] = vars['stack_min'];
+					syscallUsage[syscall]['stack_max'] = vars['stack_max'];
+					syscall++;
 				})
 				if (this.eof()) end();
 			})
 		}
+
+		console.dir(syscallUsage);
 
 		res.end();
 	})
